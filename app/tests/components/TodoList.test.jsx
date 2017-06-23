5134243a985e82538eeeef239860e9f41bf89043
Arjun  Dass <ad.sam007@gmail.com>
2017-06-22 21:28:28 -0500

    implemented test for todoList

--- app/tests/components/TodoList.test.jsx
+++ app/tests/components/TodoList.test.jsx
@@ -5,9 +5,25 @@ var expect = require('expect');
 var $ = require('jquery');
 
 var TodoList = require('TodoList');
+var Todo = require('Todo');
 
 describe('TodoList', () => {
     it('should exist', () => {
         expect(TodoList).toExist();
+    });
+
+    it('should render one Todo Component for each todo item', () => {
+        var todos = [{
+            id: 1,
+            text: 'Do Something'
+        },{
+            id: 2,
+            text: 'Check Mail'
+        }];
+
+        var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
+        var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
+
+        expect(todosComponents.length).toBe(todos.length); 
     });
 });
