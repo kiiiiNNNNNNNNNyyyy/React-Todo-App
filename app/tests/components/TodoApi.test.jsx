c43e8a93094b6630c7ddc15fbf94215777f2fd83
Arjun  Dass <ad.sam007@gmail.com>
2017-06-28 21:10:49 -0500

    Sort the todos by completed

--- app/tests/components/TodoApi.test.jsx
+++ app/tests/components/TodoApi.test.jsx
@@ -73,6 +73,11 @@ describe('TodpoApi', () => {
         it('should return uncompleted todo when showCompleted is false', () => {
             var filteredTodos = TodoApi.filterTodos(todos, false, '');
             expect(filteredTodos.length).toBe(1);
-        })
+        });
+
+        it('should sort by completed status', () => {
+            var filteredTodos = TodoApi.filterTodos(todos, true, '');
+            expect(filteredTodos[0].completed).toBe(false);
+        });
     });
 });
