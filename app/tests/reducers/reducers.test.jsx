256c7b17676864e2313c9cf661a826222981968c
Arjun  Dass <ad.sam007@gmail.com>
2017-08-19 20:52:54 -0500

    Added deep freeze library

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -1,7 +1,10 @@
 var expect = require('expect');
 var reducers = require('reducers');
+var df = require('deep-freeze-strict');
+//df freeze library makes sure that the functions we are using as reducers are pure functions in redux
 
 describe('Reducers', () => {
+
     describe('searchTextReducers', () => {
 
         it('should set searchText', () => {
@@ -10,9 +13,22 @@ describe('Reducers', () => {
                 searchText: 'dog'
             };
 
-            var res = reducers.searchTextReducer('', action);
+            var res = reducers.searchTextReducer(df(''), df(action));
 
             expect(res).toEqual(action.searchText); 
+        });
+    });
+
+    describe('showCompletedReducers', () => {
+        
+        it('should toggle showCompleted', () => {
+            var action = {
+                type: 'TOGGLE_SHOW_COMPLETED',
+            };
+
+            var res = reducers.showCompletedReducer(df(false), df(action));
+
+            expect(res).toEqual(true); 
         });
     });
 });
