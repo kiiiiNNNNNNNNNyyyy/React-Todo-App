dff76e90f46efa9782cf3295dcc376b9a140a27a
Arjun  Dass <ad.sam007@gmail.com>
2017-08-25 22:10:20 -0500

    Added fetch todos from firebase

--- app/actions/actions.jsx
+++ app/actions/actions.jsx
@@ -1,6 +1,6 @@
-import firebase, {firebaseRef} from 'app/firebase/';
 import moment from 'moment';
-import uuid from 'node-uuid'; 
+
+import firebase, {firebaseRef} from 'app/firebase/';
 
 export var setSearchText = (searchText) => {
 	return {
@@ -18,41 +18,20 @@ export var toggleShowCompleted = () => {
 export var addTodo = (todo) => {
 	return {
 		type: 'ADD_TODO',
-		todo: todo
+		todo
 	};
 };
-
-export var startAddTodos = (todos) => {
-	return (dispatch, getState) => {
-		var todosRef = firebaseRef.child('todos');
-		
-		todosRef.once('value').then((snapshot) => {
-			var todos = snapshot.val() || {};
-			var parsedTodos = [];
-
-			Object.keys(todos).forEach((todoId) => {
-				parsedTodos.push({
-					id: todoId,
-					...todos[todoId]
-				});
-			});
-
-			dispatch(addTodos(parsedTodos));
-		});
-	};
-}
 
 export var startAddTodo = (text) => {
 	return (dispatch, getState) => {
 		var todo = {
-			id: uuid(),
-			text: text,
+			text,
 			completed: false,
 			createdAt: moment().unix(),
 			completedAt: null
 		};
 		var todoRef = firebaseRef.child('todos').push(todo);
-
+		
 		return todoRef.then(() => {
 			dispatch(addTodo({
 				...todo,
@@ -69,24 +48,44 @@ export var addTodos = (todos) => {
 	};
 };
 
+export var startAddTodos = () => {
+	return (dispatch, getState) => {
+		var todosRef = firebaseRef.child('todos');
+		
+		return todosRef.once('value').then((snapshot) => {
+			var todos = snapshot.val() || {};
+			var parsedTodos = [];
+			
+			Object.keys(todos).forEach((todoId) => {
+				parsedTodos.push({
+					id: todoId,
+					...todos[todoId]
+				});
+			});
+			
+			dispatch(addTodos(parsedTodos));
+		});
+	};
+};
+
 export var updateTodo = (id, updates) => {
 	return {
 		type: 'UPDATE_TODO',
-		id: id,
-		updates: updates 
+		id,
+		updates
 	};
 };
 
 export var startToggleTodo = (id, completed) => {
 	return (dispatch, getState) => {
-		var todoRef = firebaseRef.child('todos/' + id);
+		var todoRef = firebaseRef.child(`todos/${id}`);
 		var updates = {
-			completed: completed,
-			completedAt: completed ? moment().unix() : null 
+			completed,
+			completedAt: completed ? moment().unix() : null
 		};
-
+		
 		return todoRef.update(updates).then(() => {
 			dispatch(updateTodo(id, updates));
 		});
-	}
-}
+	};
+};
--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -90,13 +90,18 @@ describe('Actions', () => {
 		var testTodoRef;
 		
 		beforeEach((done) => {
-			testTodoRef = firebaseRef.child('todos').push();
-			
-			testTodoRef.set({
-				text: 'Something to do',
-				completed: false,
-				createdAt: 23453453
-			}).then(() => done());
+			var todosRef = firebaseRef.child('todos');
+			todosRef.remove().then(() => {
+				testTodoRef = firebaseRef.child('todos').push();
+
+				return testTodoRef.set({
+					text: 'Something to do',
+					completed: false,
+					createdAt: 23453453
+				})
+			})
+			.then(() => done())
+			.catch(done);
 		});
 		
 		afterEach((done) => {
@@ -122,5 +127,20 @@ describe('Actions', () => {
 				done();
 			}, done);
 		});
+
+		it('should populate todos and dispatch ADD_TODOS', (done) => {
+			const store = createMockStore({});
+			const action = actions.startAddTodos();
+
+			store.dispatch(action).then(() => {
+				const mockActions = store.getActions();
+
+				expect(mockActions[0].type).toEqual('ADD_TODOS');
+				expect(mockActions[0].todos.length).toEqual(1);
+				expect(mockActions[0].todos[0].text).toEqual('Something to do');
+
+				done();
+			}, done)
+		})
 	});
 });
