e046662f4e3f63da5ea48422d15469d504f9fa9c
Arjun  Dass <ad.sam007@gmail.com>
2017-08-27 21:02:47 -0500

    Implemented uid for todos

--- app/actions/actions.jsx
+++ app/actions/actions.jsx
@@ -108,3 +108,16 @@ export var startLogout = () => {
 		});
 	};
 }
+
+export var login = (uid) => {
+	return {
+		type: 'LOGIN',
+		uid: uid
+	};
+}
+
+export var logout = () => {
+	return {
+		type: 'LOGOUT'
+	}
+};
--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -31,7 +31,7 @@ export var todosReducer = (state = [], action) => {
 			if (todo.id === action.id) {
 				return {
 					...todo,
-					...action.updates
+					...action	.updates
 				}
 			} else {
 				return todo;
@@ -46,3 +46,16 @@ export var todosReducer = (state = [], action) => {
 		    return state;
 	}
 };
+
+export var authReducer = (state = {}, action) => {
+	switch(action.type){
+		case 'LOGIN':
+			return {
+				uid: action.uid
+			}
+		case 'LOGOUT': 
+			return {};
+		default: 
+			return state;
+	}
+}
